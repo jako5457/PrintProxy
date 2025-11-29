07ea098fe89be316218320ff7ea4b62cc499ceb7
Jako5457 <jako5457@gmail.com>
2025-11-30 00:30:13 +0100

    Add project files.

---
+++ 3DPrintLib/IPrinter.cs
@@ -0,0 +1,55 @@
+﻿using _3DPrintLib;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PrintLib
+{
+    public interface IPrinter
+    {
+
+        /// <summary>
+        /// Eventhandler for Giving printer info live
+        /// </summary>
+        public event EventHandler<PrinterEventargs> OnStatusChanged;
+
+        /// <summary>
+        /// Prints the file. File must have been uploaded before calling this.
+        /// </summary>
+        /// <param name="fileName">The file to be printed</param>
+        /// <returns></returns>
+        public Task StartAsync(string fileName);
+
+        /// <summary>
+        /// Continues a paused print
+        /// </summary>
+        /// <returns></returns>
+        public Task ContinueAsync();
+        
+        /// <summary>
+        /// Pauses the print
+        /// </summary>
+        /// <returns></returns>
+        public Task PauseAsync();
+
+        /// <summary>
+        /// Stops The Print
+        /// </summary>
+        /// <returns></returns>
+        public Task StopAsync();
+
+        /// <summary>
+        /// Uploads the file to the printer
+        /// </summary>
+        /// <param name="FilePath"></param>
+        /// <returns></returns>
+        public Task UploadAsync(string FilePath);
+
+        /// <summary>
+        /// Gives your the printer information imidiatly
+        /// </summary>
+        /// <returns></returns>
+        public Task<PrinterEventargs> GetStatusAsync();
+
+    }
+}
