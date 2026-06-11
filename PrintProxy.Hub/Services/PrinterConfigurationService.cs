41914ed79e56f18dcdae3769437838518250f058
jako5457 <jako5457@gmail.com>
2026-06-11 16:13:04 +0200

    Added configuration fixes

--- PrintProxy.Hub/Services/PrinterConfigurationService.cs
+++ PrintProxy.Hub/Services/PrinterConfigurationService.cs
@@ -3,7 +3,7 @@ using PrintProxy.Hub.Services.Configs;
 
 namespace PrintProxy.Hub.Services
 {
-    public class PrinterConfigurationService(IConfiguration _Config) : IPrinterConfigurationService
+    public class PrinterConfigurationService(IConfiguration _Config, ILogger<PrinterConfigurationService> logger) : IPrinterConfigurationService
     {
 
         private readonly JsonSerializerSettings SerializerSettings = new JsonSerializerSettings()
@@ -21,9 +21,19 @@ namespace PrintProxy.Hub.Services
                 location = "PrinterConfig.json";
             }
 
+            logger.LogInformation("Printer configuration is " + location);
+
             if (!File.Exists(location))
             {
-                return new MainConfigEntry();
+                var conf = new MainConfigEntry();
+
+                var data = JsonConvert.SerializeObject(conf, SerializerSettings);
+
+                logger.LogInformation("Created config at " + location);
+
+                File.WriteAllText(location, data);
+
+                return conf;
             }
 
             string json = File.ReadAllText(location);
